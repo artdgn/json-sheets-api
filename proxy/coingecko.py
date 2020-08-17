89cda784d15186e6d92aa1cc74c900c2478d4198
artdgn <arthurdgn@gmail.com>
2020-08-18 08:18:41 +1000

    add client cache expiry

--- proxy/coingecko.py
+++ proxy/coingecko.py
@@ -1,3 +1,4 @@
+import time
 from typing import List, Dict
 
 import requests
@@ -6,25 +7,41 @@ import requests
 class Client:
     """
     API docs at: https://www.coingecko.com/en/api#explore-api
+
+    also possible to use https://github.com/man-c/pycoingecko instead
     """
     ADDRESS = 'https://api.coingecko.com/api/v3/'
 
+    # cache management
+    CACHE_TTL_SEC = 7 * 86400  # 7 days
+    _cache_time: float = 0
+
+    # lazy loading and class state
     _coins: List[dict] = None
     _sym_to_id: Dict[str, str] = None
     _id_to_sym: Dict[str, str] = None
 
     def _load_coins_data(self):
-        if self._coins is None:
+        if (time.time() - self._cache_time) > self.CACHE_TTL_SEC:
             self._coins = requests.get(f'{self.ADDRESS}/coins/list').json()
             self._sym_to_id = {d['symbol'].lower(): d['id'] for d in self._coins}
             self._id_to_sym = {d['id']: d['symbol'] for d in self._coins}
 
     @property
-    def symbol_id_map(self):
+    def symbol_id_map(self) -> Dict[str, str]:
+        """
+        :return: dict of ticker symbols to CoinGecko ids
+        """
         self._load_coins_data()
         return self._sym_to_id
 
     def prices_for_symbols(self, symbols: str, currency: str) -> List[float]:
+        """
+        :param symbols: list of ticker symbol strings (e.g. `btc`) that are
+            translated to CoinGecko ids (e.g. `bitcoin`) to make the requests.
+        :param currency: e.g. `usd`
+        :return: list of floats
+        """
         ids = list(map(self.symbol_id_map.get, symbols.lower().split()))
         res = requests.get(
             f'{self.ADDRESS}/simple/price',
